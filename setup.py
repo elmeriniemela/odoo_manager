b799d9cd4fbc50600ca55e8818723807ae39e2b4
Elmeri Niemela <niemela.elmeri@gmail.com>
2020-01-15 17:30:37 +0200

    Cleaning

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 setup(
     name='odoo_manager',
     packages=['odoo_manager'],
-    description = 'Wrapper to start odoo development and create new modules',
-    author = 'thecodebasesite',
-    version = '1.0',
+    description='Wrapper to start odoo development and create new modules',
+    license='GNU GPLv3',
+    author='Elmeri Niemela',
+    version='1.0',
     include_package_data=True,
     install_requires=[
         'prompt_toolkit', 'jinja2',
     ],
-    entry_points = {'console_scripts': ['odoo_manager = odoo_manager.manager:main',],},
+    entry_points={'console_scripts': ['odoo_manager = odoo_manager.manager:main',],},
 )
