7c35528c4ef0a3eaac68e2f2b1a678d88ee1eeb5
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-10-16 21:08:22 +0300

    update template

--- odoo_manager/tests/test_template.py
+++ odoo_manager/tests/test_template.py
@@ -4,7 +4,7 @@ from odoo_manager.template import ModuleTemplate
 from odoo_manager.manager import get_config
 
 template = ModuleTemplate(get_config())
-template.name = 'sprintit_test'
+template.name = 'odoo_test'
 template.add('models', 'account.invoice')
 template.add('models', 'account.move.line')
 template.add('wizards', 'download.account.statements')
